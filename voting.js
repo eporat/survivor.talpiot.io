99846d341a48a913c68cfcfb5e74a208e512bb8d
Eitan Porat <ethan.porat@gmail.com>
2020-04-18 23:43:27 +0300

    added design

--- voting.js
+++ voting.js
@@ -13,20 +13,23 @@ firebase.initializeApp(firebaseConfig);
 
 const database = firebase.firestore();
 const usersRef = database.collection("users");
+const votesRef = database.collection("votes");
+
 const user = JSON.parse(localStorage.getItem("userData"));
 
 const clan = user.clan;
 
 console.log(user);
 
-function addRadioButton(text) {
+function addRadioButton(data) {
+    var text = data.name;
     var div = document.getElementById("clan-members");
     var button = document.createElement("button");
     var label = document.createElement("label");
     var element = document.createElement("input");
     //Assign different attributes to the element.
     element.setAttribute("type", "radio");
-    element.setAttribute("value", "בחרת ב" + text);
+    element.setAttribute("value", data.name);
     element.setAttribute("name", "optradio");
     element.setAttribute("class", "radio");
     label.className = "d-block";
@@ -45,10 +48,37 @@ usersRef
 .then((querySnapshot) => {
     querySnapshot.forEach((doc) => {
         const data = doc.data();
-        if (data.email != user.email){
-            addRadioButton(data.name);
+        if (data.email != user.email && !data.immunity){
+            addRadioButton(data);
         }
     })
 })
 
 
+async function vote(){
+    const voted = document.querySelector('input:checked').value;
+    let numberOfVotes = 0;
+    let voters = [];
+    let userDoc = await usersRef.doc(user.firstName + "-" + user.lastName).get()
+    let userVoted = userDoc.data().voted;
+
+    if (!userVoted){
+        const doc = await votesRef.doc(voted).get()
+        const data = doc.data();
+        if (data){
+            numberOfVotes = 0 || data.votes;
+            console.log(doc);
+            console.log(numberOfVotes);
+            voters = data.voters || [];
+        }
+        
+        if (!voters.includes(user.name)){
+            voters.push(user.name);
+            votesRef
+            .doc(voted)
+            .set({"votes": numberOfVotes + user.numberOfVotes, "voters": voters})
+        }
+
+        usersRef.doc(user.firstName + "-" + user.lastName).update({"voted": true});
+    }
+}
