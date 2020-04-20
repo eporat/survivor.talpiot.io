9d86123d3ea418da9a00ef43c5068618cf8ff380
Eitan Porat <ethan.porat@gmail.com>
2020-04-20 09:04:18 +0300

    changes

--- user.js
+++ user.js
@@ -2,6 +2,6 @@ const user = JSON.parse(localStorage.getItem("userData"));
 const clans = {"a": "א", "b": "ב", "c": "ג"};
 const colors = {"a": "blue", "b": "red", "c": "green"}
 document.getElementById("name").innerText = user.name;
-document.getElementById("clan").innerText = "שבט: " + `<span id="clan-color">${clans[user.clan]}<\span>`;
+document.getElementById("clan").innerHTML = "שבט: " + `<span id="clan-color">${clans[user.clan]}<\span>`;
 document.getElementById("clan-color").style.color = colors[user.clan];
 document.getElementById("unit").innerText = "צוות: " + user.unit;
